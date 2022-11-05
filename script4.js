d0ce425e7bdd8a9bf95f01948774b535e7525638
Olamphzy <61218900+Olamphzy@users.noreply.github.com>
2022-11-05 13:21:13 +0100

    Update script4.js

--- script4.js
+++ script4.js
@@ -10,10 +10,10 @@ const animated_bg_texts = document.querySelectorAll('.animated-bg-text');
 
 setTimeout(getData, 2500)
 function getData() {
-    header.innerHTML = '<img src="/images/cool.jpg" alt="cool" />'
+    header.innerHTML = '<img src="cool.jpg" alt="cool" />'
     title.innerHTML = 'My Frontend Contact Card'
     except.innerHTML = "I'm a web Developer with extensive experience of a year. I'm skilled in HTML, CSS, JavaScript, NestJs and some framework such as React & Reduxjs, AngularJs & Nestjs and nodejs. My expertise is to create and develop web design, game development, graphic designer, animations and software engineering. Hit the dial to contact me!"
-    profile_img.innerHTML = '<img src="/images/Olamphzy.png" alt="Profile Pic" />'
+    profile_img.innerHTML = '<img src="Olamphzy.png" alt="Profile Pic" />'
     person.innerHTML = 'Olanrewaju Rasheed'
     contact.innerHTML = 'Olanrewajurasheed03@gmail.com'
 
